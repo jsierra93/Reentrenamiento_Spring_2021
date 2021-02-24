5ab819af3c01ff6fcf402c6ac2d9551057f1a646
jose.jaramillo <jose0044@gmail.com>
2021-02-24 14:53:38 -0500

    Setup inicial

---
+++ src/test/java/com/curso/parqueDiversiones/ParqueDiversionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.parqueDiversiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueDiversionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
